ab399ed377582a506330e222077c5541ea765d6e
Shiv Kumar <yourshivkumar17@gmail.com>
2025-08-08 08:55:56 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+    public void displayInfo() {
+        System.out.print("Name: Aman Sharma");
+        System.out.print("Age: 16");
+        System.out.print("Grade: 10th");
+
+    }
+    public static void main(String[] args) {
+        StudentInfo sc = new StudentInfo();
+        sc.displayInfo();
+    }
+}
